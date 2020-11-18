176261da00bbcc28bf923b088f411e0d6c31b6ac
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-11-18 14:21:20 +0200

    set folder structure

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import User from './User/User';
+
+class App extends Component {
+  state = {
+    users: [
+      {name: 'Nadiia', email: 'nadiia@gmail.com'},
+      {name: 'Max', email: 'max@gmail.com'},
+      {name: 'Taras', email: 'taras@gmail.com'}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      users: [
+        {name: 'Nadiia', email: 'nadiia@gmail.com'},
+        {name: 'Max', email: 'max@gmail.com'},
+        {name: 'Lesia', email: 'lesia@gmail.com'}
+      ]
+    })
+  }
+
+  render() {
+    return (
+    <div className="App">
+      <h1>This is my webapp</h1>
+      <p>It's awesome!</p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <User 
+      name={this.state.users[0].name} 
+      email={this.state.users[0].email} 
+      />
+      <User 
+      name={this.state.users[1].name} 
+      email={this.state.users[1].email}>Hobbies: Football
+      </User>
+      <User 
+      name={this.state.users[2].name} 
+      email={this.state.users[2].email} 
+      />
+    </div>
+  );
+  }
+}
+
+export default App;
