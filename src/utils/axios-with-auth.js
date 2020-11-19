d3c3c6d4266fbfc389b456d85b0732f37533ce6a
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-11-19 10:01:50 +0200

    configured axios

--- src/utils/axios-with-auth.js
+++ src/utils/axios-with-auth.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import isomorphicCookie from 'isomorphic-cookie';
-import * as JWT from 'jwt-decode';
-import history from '../history';
-import { apiURL } from '../constants';
+// import axios from 'axios';
+// import isomorphicCookie from 'isomorphic-cookie';
+// import * as JWT from 'jwt-decode';
+// import history from '../history';
+// import { apiURL } from '../constants';
 
-const authHeader = {
-  Authorization: isomorphicCookie.load('token')
-    ? `Bearer ${isomorphicCookie.load('token')}`
-    : null,
-};
+// const authHeader = {
+//   Authorization: isomorphicCookie.load('token')
+//     ? `Bearer ${isomorphicCookie.load('token')}`
+//     : null,
+// };
 
-const authorize = response => {
-  if (response.Message) {
-    isomorphicCookie.remove('token');
-    history.push('/login');
-  }
-};
+// const authorize = response => {
+//   if (response.Message) {
+//     isomorphicCookie.remove('token');
+//     history.push('/login');
+//   }
+// };
 
-export default {
-  async get(url, data) {
-    this.setHeader();
-    const response = await axios.get(url, {
-      headers: authHeader,
-      params: data,
-    });
-    authorize(response.data);
-    return response;
-  },
+// export default {
+//   async get(url, data) {
+//     this.setHeader();
+//     const response = await axios.get(url, {
+//       headers: authHeader,
+//       params: data,
+//     });
+//     authorize(response.data);
+//     return response;
+//   },
 
-};
+// };
